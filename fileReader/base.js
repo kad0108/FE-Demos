3cc20ecd8f9626513efc75748f8ec38c0fb97e89
kad0108 <kad0108@foxmail.com>
2016-04-26 22:00:51 +0800

    filereader

---
+++ fileReader/base.js
@@ -0,0 +1,12 @@
+function $(id){
+	return document.getElementById(id);
+}
+function addEvent(element, eventName, listener) {
+    if (element.addEventListener) {
+        element.addEventListener(eventName, listener, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + eventName, listener);
+    } else {
+        element["on" + eventName] = listener;
+    }
+}
