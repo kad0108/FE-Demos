87cf910238a713171c05c7dc171b63f88d94d3ff
kad0108 <kad0108@foxmail.com>
2016-04-29 10:34:47 +0800

    gmap test

--- geolocation/gmap.js
+++ geolocation/gmap.js
@@ -18,6 +18,12 @@ function displayLocation(position){
 	var div = $("location");
 	console.log(div);
 	div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+
+	var km = computeDistance(position.coords, ourCoords);
+	var distance = $("distance");
+	distance.innerHTML = "You are " + km + " km from the HQ";
+
+	showMap(position.coords);
 }
 
 function displayError(error){
@@ -33,4 +39,65 @@ function displayError(error){
 	}
 	var div = $("location");
 	div.innerHTML = errorMessage;
+}
+//计算距离
+function computeDistance(startCoords, destCoords){
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+
+	var Radius = 6371;//km
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + 
+					Math.cos(startLatRads) * Math.cos(destLatRads) * 
+					Math.cos(startLongRads - destLongRads)) * Radius;
+
+	return distance;
+}
+
+var ourCoords = {
+	latitude: 47.624851,
+	longitude: -122.52099
+};
+
+function degreesToRadians(degrees){
+	var radians = (degrees * Math.PI)/180;
+	return radians;
+}
+
+//位置地图
+var map;
+function showMap(coords){
+	var googleLatAndLong = new google.maps.LatLng(coords.latitude, coords.longitude);
+	var mapOptions = {
+		zoom: 10,
+		center: googleLatAndLong,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var mapDiv = $("map");
+	map = new google.maps.Map(mapDiv, mapOptions);
+
+	var title = "Your Location";
+	var content = "You are here: " + coords.latitude + ", " + coords.longitude;
+	addMarker(map, googleLatAndLong, title, content);
+}
+
+//加大头钉
+function addMarker(map, latlong, title, content){
+	var markerOptions = {
+		position: latlong,
+		map: map,
+		title: title,
+		clickable: true
+	};
+	var marker = new google.maps.Marker(markerOptions);
+
+	var infoWindowOptions = {
+		content: content,
+		position: latlong
+	};
+	var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+	google.maps.event.addListener(marker, "click", function(){
+		infoWindow.open(map);
+	});
 }
