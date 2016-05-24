46876cdccb0d970df41ce0a37a00e8d40c8bc2b8
kad0108 <kad0108@foxmail.com>
2016-05-24 09:29:17 +0800

    kad

--- webThread/manager.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-	var worker = new Worker("worker.js");
-	console.log(worker);
-	worker.postMessage("Say");
-	worker.onmessage = function(event){
-		var message = "Worker says " + event.data;
-		$("output").innerHTML = message;
-	};
-}
--- webThread/worker.js
+++
@@ -1,7 +0,0 @@
-onmessage = sayCheese;
-function sayCheese(event){
-	console.log("worker", event);
-	if(event.data == "Say"){
-		postMessage("Cheese");
-	}
-}
